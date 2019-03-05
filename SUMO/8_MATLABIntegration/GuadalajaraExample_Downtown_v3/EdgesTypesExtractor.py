bebe3bb406f24482776324249bd61903636a879d
Alberto Briseno <jabl91@gmail.com>
2019-03-04 22:12:28 -0600

    Doxy comments have been added to two modules.
    EdgesTypesExtractor and Geometry python modules have been  doxy commented.

--- SUMO/8_MATLABIntegration/GuadalajaraExample_Downtown_v3/EdgesTypesExtractor.py
+++ SUMO/8_MATLABIntegration/GuadalajaraExample_Downtown_v3/EdgesTypesExtractor.py
@@ -1,10 +1,30 @@
+
+## @package EdgesTypesExtractor
+#  This module implements the extraction of features of the network netFile
+#  which is given by the SUMO netRouter. This extraction is done before
+#  the execution of the simulation, this reduces heavily the runtime
+#  processing that is needed.
+
+
 from __future__ import absolute_import
 from __future__ import print_function
 import xml.etree.ElementTree as ET
 
-import traci
+# import traci
 from myPythonDependencies.myConstants import myConsts
 from Geometry import GeometryClass
+
+
+## This the main class that implements all the methods to extract
+#  the information from the netFile.
+#  @remark Cyclepath is defined by (Snizek et al 2013) as
+#  "we have classified the cycling infrastructure in the following
+#  manner: street with no cycling facilities; cycle path by the
+#  side of the road (separated from motor traffic by a curb); cycle lane
+#  on the road (separated from motor traffic by markings on the
+#  road); off-road path exclusively for cyclists, and finally off-road
+#  path shared by cyclists and pedestrians.
+
 
 class EdgeTypesExtractor:
 
@@ -12,70 +32,91 @@ class EdgeTypesExtractor:
     STR_HIGHWAY = 'highway'
     STR_RAILWAY = 'railway'
 
-    # cyclepath is defined by (Snizek et al 2013) as
-    # "we have classified the cycling infrastructure in the following
-    # manner: street with no cycling facilities; cycle path by the
-    # side of the road (separated from motor traffic by a curb); cycle lane
-    # on the road (separated from motor traffic by markings on the
-    # road); off-road path exclusively for cyclists, and finally off-road
-    # path shared by cyclists and pedestrians.
-
+    ## This is a dictionary that converts all the OpenStreetMaps (R)
+    #  edge descriptors into a minimal subset of owned defined descriptors
+    #  this will simplify the analysis and it will make them match them
+    #  descriptors that are expected by the CopenhagenABM agent model.
     DECISION_CONVER_DICT = {
-                    'cycleway.lane': 'cyclepath',
-                    'cycleway.opposite_lane': 'cyclepath',
-                    'cycleway.opposite_track': 'cyclepath',
-                    'cycleway.track': 'cyclepath',
-                    'highway.bridleway': 'residential',
-                    'highway.bus_guideway': 'residential',
-                    'highway.cycleway': 'cyclepath',
-                    'highway.footway': 'residential',
-                    'highway.ford': 'residential',
-                    'highway.living_street': 'residential',
-                    'highway.motorway': 'residential',
-                    'highway.motorway_link': 'residential',
-                    'highway.path': 'path',
-                    'highway.pedestrian': 'residential',
-                    'highway.primary': 'housingWshops',
-                    'highway.primary_link': 'housingWshops',
-                    'highway.raceway': 'residential',
-                    'highway.residential': 'residential',
-                    'highway.secondary': 'secondary',
-                    'highway.secondary_link': 'secondary',
-                    'highway.service': 'secondary',
-                    'highway.stairs': 'residential',
-                    'highway.step': 'residential',
-                    'highway.steps': 'residential',
-                    'highway.tertiary': 'secondary',
-                    'highway.tertiary_link': 'secondary',
-                    'highway.track': 'residential',
-                    'highway.trunk': 'residential',
-                    'highway.trunk_link': 'residential',
-                    'highway.unclassified': 'residential',
-                    'highway.unsurfaced': 'residential',
-                    'railway.light_rail': 'railway',
-                    'railway.preserved': 'railway',
-                    'railway.rail': 'railway',
-                    'railway.subway': 'railway',
-                    'railway.tram': 'railway'}
+        'cycleway.lane': 'cyclepath',
+        'cycleway.opposite_lane': 'cyclepath',
+        'cycleway.opposite_track': 'cyclepath',
+        'cycleway.track': 'cyclepath',
+        'highway.bridleway': 'residential',
+        'highway.bus_guideway': 'residential',
+        'highway.cycleway': 'cyclepath',
+        'highway.footway': 'residential',
+        'highway.ford': 'residential',
+        'highway.living_street': 'residential',
+        'highway.motorway': 'residential',
+        'highway.motorway_link': 'residential',
+        'highway.path': 'path',
+        'highway.pedestrian': 'residential',
+        'highway.primary': 'housingWshops',
+        'highway.primary_link': 'housingWshops',
+        'highway.raceway': 'residential',
+        'highway.residential': 'residential',
+        'highway.secondary': 'secondary',
+        'highway.secondary_link': 'secondary',
+        'highway.service': 'secondary',
+        'highway.stairs': 'residential',
+        'highway.step': 'residential',
+        'highway.steps': 'residential',
+        'highway.tertiary': 'secondary',
+        'highway.tertiary_link': 'secondary',
+        'highway.track': 'residential',
+        'highway.trunk': 'residential',
+        'highway.trunk_link': 'residential',
+        'highway.unclassified': 'residential',
+        'highway.unsurfaced': 'residential',
+        'railway.light_rail': 'railway',
+        'railway.preserved': 'railway',
+        'railway.rail': 'railway',
+        'railway.subway': 'railway',
+        'railway.tram': 'railway'}
 
     cycleway_set = []
     highway_set = []
     railway_set = []
 
+    ## The constructor.
+    #  @param netFile represents the xml file that contains all the network
+    #         information that is provided by SUMO. The default name of
+    #         this file is 'osm.net.xml'
     def __init__(self, netFile):
+        ## @var tree
+        #  XML Tree pointer
         self.tree = ET.parse(netFile)
 
+    ##  This internal method returns the tree pointer of the xml file
+    #   @param self The object pointer.
     def __getTree(self):
         return self.tree
 
+    ##  This internal method returns the tree root pointer of the xml file
+    #   @param self The object pointer.
     def __getRoot(self):
         return self.tree.getroot()
 
+    ## This method extracts all the possible edges descriptors
+    #  on regards to a classification of 3 elements (highway, railway or
+    #  cicleway). Once it extracts all the combinations that exist
+    #  on the netFile it will remove all the duplicated elements
+    #  and it will provide a dictionary of all the sets of options
+    #  that are associated to the 3 elements mentioned before.
+    #  @param self The object pointer.
+    #  @return A dictionary that contains all the possible edge
+    #          descriptors regarding three different classification classes,
+    #          highway, railway or cicleway.
     def findAllAvailableTypes(self):
+
+        # Temporary lists that will be used to categorize the
+        # sets of edge descriptors.
         cycleway_types = []
         highway_types = []
         railway_types = []
 
+        # All different types of edge descriptors will be unclassified
+        # among three different elements (cycleway, highway and railway)
         for myType in self.__getRoot().iter('type'):
             currentID = myType.attrib.get('id')
             if self.STR_CYCLEWAY in currentID:
@@ -85,10 +126,26 @@ class EdgeTypesExtractor:
             if self.STR_RAILWAY in currentID:
                 railway_types.append(currentID)
 
+        # All different types of edges descriptors will be converted into
+        # sets in order to be able to apply operations like union, intersection
+        # and difference.
+        ## @var cycleway_set
+        #  Set of all the edge types that are related to a cycleway.
         self.cycleway_set = set(cycleway_types)
+        ## @var highway_set
+        #  Set of all the edge types that are related to a highway.
         self.highway_set = set(highway_types)
+        ## @var railway_set
+        #  Set of all the edge types that are related to a railway.
         self.railway_set = set(railway_types)
 
+        # All the same set elements between cycleway set and highway set
+        # will be identified and the duplicates will be deleted from highway
+        # set. This is done on purpose so every time a there is a highway
+        # edge that contains a dedicated lane for cycleways, then it would
+        # be easier from the implementation perspective to just consider it
+        # as a cycleway and assign its associated weight into the
+        # multinomial linear regression model
         for match in self.cycleway_set & self.highway_set:
             self.highway_set.remove(match)
 
@@ -98,12 +155,21 @@ class EdgeTypesExtractor:
         for match in self.highway_set & self.railway_set:
             self.railway_set.remove(match)
 
+        # A dictionary is returned by the function which contains all the
+        # possible sets of values that can be found within the netFile
+        # grouped by the 3 descriptors that are expected by the agent model.
         waysDict = {self.STR_CYCLEWAY: self.cycleway_set,
                     self.STR_HIGHWAY: self.highway_set,
                     self.STR_RAILWAY: self.railway_set}
 
         return waysDict
 
+    ## This method is an improved analysis to retrieve the edge
+    #  descriptor of every of the possible edges that are contained within
+    #  the netFile. Instead of creating a dictionary during runtime,
+    #  an already defined table #DECISION_CONVER_DICT is used to create
+    #  a dictionary that maps the name of the edge to its corresponding
+    #  agent model edge descriptor.
     def getEdgeTypeDict(self):
         # self.findAllAvailableTypes()
         EdgeToEdgeType = {}
@@ -112,16 +178,42 @@ class EdgeTypesExtractor:
                 currentTypeSet = myEdge.attrib.get('type')
                 if(self.DECISION_CONVER_DICT[currentTypeSet]):
                     EdgeToEdgeType[myEdge.attrib.get('id')] = \
-                            self.DECISION_CONVER_DICT[currentTypeSet]
+                        self.DECISION_CONVER_DICT[currentTypeSet]
                 else:
                     EdgeToEdgeType[myEdge.attrib.get('id')] = None
 
         return EdgeToEdgeType
 
+    ## This method iterates over all the edges that are defined on the
+    #  netFile and returns two dictionaries, one that contains all the edge
+    #  centers and the other one which contains the corresponding slope
+    #  and intercept of every of those edges.
+    # @return One dictionary that has all the netFile edge names as
+    #          keys and the values are the center of those edges.
+    # @return  Another dictonary that has all the netFile edge names
+    #          as keys and the values as the slope and intercept that
+    #          describes those edges based on the global map coordinates
+    #  @sa #GeometryClass
     def getCenterofEdgeDict(self):
+        # Geometry Class implements several trgonometrics APIs
+        # that will be ued within this function to find the edge
+        # centers as well as the linear equation that describes each of them
         myGeometryAPI = GeometryClass()
+
+        # These two dictionaries are initialized and each of them will
+        # represent the output of the function
         EdgetoEdgeCenter = {}
         EdgeLinearEquation = {}
+
+        # The netFile will be analyzed and all defined edges will be iterated.
+        # It is assumed that a single lane will be sufficient to calculate
+        # the center of the edge. However, this assumption creates a small
+        # center offset that will be inherent to the final value.
+        # A better implementation would be to take the center of all the lanes
+        # that are attached to an edge and then average them to get the
+        # true edge center.
+        # Based on the actual difference, the former algorithm works fine
+        # for the agent model
 
         for myEdge in self.__getRoot().iter('edge'):
             if(myEdge.findall('lane')):
@@ -147,6 +239,13 @@ class EdgeTypesExtractor:
 
         return EdgetoEdgeCenter, EdgeLinearEquation
 
+    ## This method returns all the "outlets" that are associated to an edge
+    #  that enters an intersection. In other words, if a vehicle arrives to a
+    #  intersection, it would have to choose between all the possible edges in
+    #  which it can continue its movement
+    #  @return A dictionary which keys maps to every edge on the Netfile and
+    #          their values are all the options that a car has once it arrives
+    #          into an intersection
     def getEdgeToInsersectionDict(self):
         EdgeToEdgeConnection = {}
         for myRoadConnection in self.__getRoot().iter('connection'):
@@ -160,6 +259,18 @@ class EdgeTypesExtractor:
         # print(EdgeToEdgeConnection)
         return EdgeToEdgeConnection
 
+
+    ## This method calculates the direction of the vehicle's movement
+    #  when a edge has been taken after entering an intersection
+    #  this information is highly relevant for the agent model decision
+    #  matrix.
+    #  @return A dictionary which keys are all the edges that are defined
+    #          within the netFile. Their values are the "outlet" edges
+    #          that are associated to the corresponding "inlet" edge
+    #          intersection.
+    #  @remark The return value of this method should be ordered with
+    #          the one provided by #getEdgeToInsersectionDict, so a correct
+    #          mapping can be inferred.
     def getEdgeToIntersection_DestType(self):
 
         EdgeToEdgeDstType = {}
@@ -176,8 +287,8 @@ class EdgeTypesExtractor:
 
 
 # treeNet = ET.parse('osm.net.xml')
-myExtractor = EdgeTypesExtractor('osm.net.xml')
-myExtractor.getEdgeToInsersectionDict()
+# myExtractor = EdgeTypesExtractor('osm.net.xml')
+# myExtractor.getEdgeToInsersectionDict()
 
 # print(myExtractor.getCenterofEdgeDict())
 # print(myExtractor.getEdgeTypeDict())
--- SUMO/8_MATLABIntegration/GuadalajaraExample_Downtown_v3/Geometry.py
+++ SUMO/8_MATLABIntegration/GuadalajaraExample_Downtown_v3/Geometry.py
@@ -1,11 +1,11 @@
 
-# This file contains the required methods to calculate which
+## @package Geometry
+# This package contains a class with the required methods to calculate which
 # routes are better than others taking into consideration
 # the weights table proposed on Mapping cyclists' experiences
 # and agent-based modelling of their wayfinding behaviour(Snizek, 2015)
 # The analysis is also based in that project Git Repo that is located at
 # https://github.com/bsnizek/CopenhagenABM
-
 # @file    Geometry.py
 # @author  Alberto Briseno
 # @date    2019-02-04
@@ -15,15 +15,27 @@ import math
 import traci
 
 
+## It provides most of the trigonometric APIs that are needed to extract
+#  useful information from the simulation edge network
 class GeometryClass:
 
+    ## This method implements an trigonmetric formula that calculates
+    #  the intercept and slope of a linear equation based on two of
+    #  its points.
+    #  @param Point1 First set of X,Y location
+    #  @param Point2 Second set of X,Y Location
+    #  @param zeroDivision In case a zero division is going to be
+    #         executed, this value will be used instead of making that
+    #         calculation
+    #  @return Slope of the linear equation
+    #  @return Intercept of the linear equation
     def getSlopeandIntercept(self, Point1, Point2, zeroDivision=None):
         slope = None
         intercept = None
 
         if(Point1[0] != Point2[0]):
             slope = (Point1[1] - Point2[1]) / \
-                     (Point1[0] - Point2[0])
+                    (Point1[0] - Point2[0])
         else:
             if(zeroDivision):
                 slope = zeroDivision
@@ -34,6 +46,10 @@ class GeometryClass:
 
         return slope, intercept
 
+    ## This method calculates the pitagoras distance between two points
+    #  @param Point1 First set of X,Y location
+    #  @param Point2 Second set of X,Y location
+    #  @return Distance between two given points
     def getDistance(self, Point1, Point2):
         distance = None
         distance = math.sqrt(
@@ -41,6 +57,12 @@ class GeometryClass:
             pow((Point1[0] - Point2[0]), 2))
         return distance
 
+    ## This method is just one way to get the maximum value that will
+    #  exist within the global map boundaries. This value is usually used
+    #  when a relatively big value is needed. (e.g. Distant point on
+    #  calculation of the angle between two vectors)
+    #  @return A relatively large value based on the largest distance
+    #          of the Y Axis of the Global Map
     def getBoundary_Y(self):
         traciBoundary_Y = traci.gui.getBoundary()
         traciBoundary_Y = \
@@ -48,6 +70,17 @@ class GeometryClass:
             abs(traciBoundary_Y[1][1])
         return traciBoundary_Y
 
+    ## This method implements the algorithm that calculates the
+    #  angle between two vectors. This two vectors can be referenced
+    #  to a reference vector, if this is not defined, it will be assumed
+    #  that both vectors are attached to coordinates [0, 0]
+    #  @param Vector1 The X,Y set that represents the direction of the first
+    #                 vector based on the ref param value
+    #  @param Vector2 The X,Y set that represents the direction of the second
+    #                 vector based on the ref param value
+    #  @param ref     The reference to which each of the previous two sets
+    #                 of points will be evaluated to while calculating their
+    #                 corresponding angles
     def getAngleBetweenVectors(self, Vector1, Vector2, ref=[0, 0]):
         # print('\ngetAngleBetweenVectors')
         # print(Vector1)
